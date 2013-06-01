4e5a8172010929107a727296fa1fa6bb5a20ba21
Ryan Cromwell <ryan@echelontouch.com>
2013-06-01 16:17:33 -0400

    some initial specs for quantities and vest behavior

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,46 @@
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality
+  beforeEach( function() {
+    initialize();
   });
+
+  it("initial qualities are known", function() {
+    expect(items[0].quality).toBe(20);
+    expect(items[1].quality).toBe(0);
+    expect(items[2].quality).toBe(7);
+    expect(items[3].quality).toBe(80);
+    expect(items[4].quality).toBe(20);
+    expect(items[5].quality).toBe(6);
+  });
+
+  describe("vests", function() {
+    it("vest goes to 19 after 1 update", function() {
+      update_quality()
+      expect( items[0].quality).toBe(19)
+    });
+    
+    it("vests never go negative", function() {
+      for(var index = 0; index < 25; index++) {
+        update_quality();
+      };
+
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("vests deteriorate at 2 after 10 days", function() { 
+      times(10, update_quality);
+      expect(items[0].quality).toBe(10);
+      update_quality();
+      expect(items[0].quality).toBe(8);
+      update_quality();
+      expect(items[0].quality).toBe(6);
+    });
+  });
+
+  function times(number, func) {
+    for(var index = 0; index < number; index++) {
+      func();
+    }
+  };
 
 });
