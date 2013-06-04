ce59f47c571d37862fbe0a2423e21185d1ab0a14
Ryan Cromwell <ryan@echelontouch.com>
2013-06-03 23:40:53 -0400

    sulfuras are known

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,41 +1,49 @@
 describe("Gilded Rose", function() {
 
+  var sulfuras = null,
+      vest = null;
+
   beforeEach( function() {
     init();
+
+    vest = items[0];
+    sulfuras = items[3];
   });
 
-  describe("initially", function() {
-    it("qualities are known", function() {
-      expect(items[0].quality).toBe(20);
-      expect(items[1].quality).toBe(0);
-      expect(items[2].quality).toBe(7);
-      expect(items[3].quality).toBe(80);
-      expect(items[4].quality).toBe(20);
-      expect(items[5].quality).toBe(6);
+  it("qualities are known", function() {
+    expect(vest.quality).toBe(20);
+    expect(items[1].quality).toBe(0);
+    expect(items[2].quality).toBe(7);
+    expect(sulfuras.quality).toBe(80);
+    expect(items[4].quality).toBe(20);
+    expect(items[5].quality).toBe(6);
+  });
+
+  describe("vests", function() {
+    it("go to 19 after 1 update", function() {
+      update_quality()
+      expect(vest.quality).toBe(19)
+    });
+    
+    it("never go negative", function() {
+      times(25, update_quality);
+      expect(vest.quality).toBe(0);
     });
 
-    describe("vests", function() {
-      it("go to 19 after 1 update", function() {
-        update_quality()
-        expect( items[0].quality).toBe(19)
-      });
-      
-      it("never go negative", function() {
-        for(var index = 0; index < 25; index++) {
-          update_quality();
-        };
+    it("deteriorate at 2 after 10 days", function() { 
+      times(10, update_quality);
+      expect(vest.quality).toBe(10);
+      update_quality();
+      expect(vest.quality).toBe(8);
+      update_quality();
+      expect(vest.quality).toBe(6);
+    });
+  });
 
-        expect(items[0].quality).toBe(0);
-      });
-
-      it("deteriorate at 2 after 10 days", function() { 
-        times(10, update_quality);
-        expect(items[0].quality).toBe(10);
-        update_quality();
-        expect(items[0].quality).toBe(8);
-        update_quality();
-        expect(items[0].quality).toBe(6);
-      });
+  describe("sulfuras", function() {
+    it("never degrades", function() {
+      times(100, update_quality);
+      expect(sulfuras.quality).toBe(80);
     });
   });
 
